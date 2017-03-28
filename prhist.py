b89acc8b680f8f2ecc9905f48c26944bcf569a5c
Oord van den <G.vandenOord@gmail.com>
2017-03-28 12:28:41 +0000

    Added monthly data aggregation to script

--- prhist.py
+++ prhist.py
@@ -40,45 +40,41 @@ def get_index_box(ds,latlonbox):
     return {"imin":imin,"imax":imax,"jmin":jmin,"jmax":jmax}
 
 
-def get_prc(path,month,day,latlonbox):
+def get_prc_day(path,month,day,latlonbox):
     files = [f for f in map(lambda p : os.path.join(path,p),os.listdir(path)) if os.path.isfile(f)]
     ncfiles = [f for f in files if os.path.splitext(f)[1] == ".nc"]
-    prcyr = {}
+    prctot = []
     for ncf in ncfiles:
+        print "Reading file",ncf,"..."
         ds = netCDF4.Dataset(ncf,'r')
         timvar = get_time_variable(ds)
         if(not timvar):
             print "Could not detect time variable in file",ncf
             continue
         times = netCDF4.num2date(timvar[:],units=timvar.units,calendar=timvar.calendar)
-        indices = []
+        indices = {}
         for i in range(len(timvar)):
-            if(times[i].month == month and times[i].day == day): indices.append((i,times[i].year))
+            if(times[i].month == month and (day <= 0 or times[i].day == day)):
+                y,d = times[i].year,times[i].day
+                if((y,d) in indices): indices[(y,d)].append(i)
+                else: indices[(y,d)] = [i]
         if(not any(indices)):
-            print "Could not find date",month,"-",day,"in time axis for file",ncf
+            print "Could not find date",month,"-",(day if day > 0 else "*"),"in time axis for file",ncf
             continue
+        print "Number of timestamps:",len(indices)
         indexbox = get_index_box(ds,latlonbox)
         prcvar = get_prc_variable(ds)
-        prctot = {}
-        for ind in indices:
-            val = numpy.mean(prcvar[ind[0],indexbox["jmin"]:indexbox["jmax"],indexbox["imin"]:indexbox["imax"]])
-            if(ind[1] in prctot):
-                prctot[ind[1]] += val
-            else:
-                prctot[ind[1]] = val
-        for k,v in prctot.iteritems():
-            if(k in prcyr):
-                prcyr[k].append(v)
-            else:
-                prcyr[k] = [v]
-    return prcyr
+        for (k,v) in indices.iteritems():
+            prtot = 0.
+            for i in v:
+                prtot += numpy.mean(prcvar[i,indexbox["jmin"]:indexbox["jmax"],indexbox["imin"]:indexbox["imax"]])
+            date = datetime.date(year=k[0],month = month,day = k[1])
+            prctot.append((date,1000*prtot))
+    return prctot
 
 
-def plot_prc_hist(prcyr):
-    vals = []
-    for k,v in prcyr.iteritems():
-        vals.extend(v)
-    plt.hist(vals,20)
+def plot_prc_hist(vals):
+    plt.hist([v[1] for v in vals],20)
     plt.show()
 
 
@@ -86,12 +82,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Draw precipitation histogram")
     parser.add_argument("--path",dest = "path",help = "<Required> Data directory",required = True)
     parser.add_argument("--month",dest = "month",help = "<Required> Histogram month",required = True)
-    parser.add_argument("--day",dest = "day",help = "<Required> Histogram day",required = True)
+    parser.add_argument("--day",dest = "day",help = "<Optional> Histogram day")
     parser.add_argument("--box",dest = "box",nargs = '+',type = float,help = "<Optional> bounding box: latmin lonmin latmax lonmax")
     args = parser.parse_args()
     path = args.path
     month = int(args.month)
-    day = int(args.day)
+    daystr = getattr(args,"day")
+    day = int(daystr) if daystr else 0
     box = getattr(args,"box",None)
     boxdict = None if box == None else {}
     boxargs = 0 if box == None else len(box)
@@ -99,6 +96,6 @@ if __name__ == "__main__":
     if(boxargs > 1): boxdict["lonmin"] = box[1]
     if(boxargs > 2): boxdict["latmax"] = box[2]
     if(boxargs > 3): boxdict["lonmax"] = box[3]
-    data = get_prc(path,month,day,boxdict)
-#    print "The data is",data
+    data = get_prc_day(path,month,day,boxdict)
+    print "Number of collected data points:",len(data)
     plot_prc_hist(data)
