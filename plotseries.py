d9805a47f5476fdbfb2ee4895f7ad7c85bb8ff20
gvdoord <g.vandenOord@gmail.com>
2017-09-11 23:56:32 +0200

    Improved plume plotting

--- plotseries.py
+++ plotseries.py
@@ -5,13 +5,13 @@ import argparse
 import numpy
 import ensoutput
 
-def plotseries(ensemble_output,box):
+def plotseries(ensemble_output,box,pltvars):
     if(not ensemble_output):
         raise Exception("Ensemble output path has not been specified")
     members = ensemble_output.get_members()
     if(len(members) < 1):
         print "No ensemble members found in " + ensemble_output.path
-    variables = ensemble_output.get_variables()
+    variables = [v for v in pltvars if v in ensemble_output.get_variables()]
     fig,axes = plt.subplots(2,len(variables))
     for i in range(len(variables)):
         axes[0,i].ticklabel_format(style='sci', axis='y', scilimits=(-2,2))
@@ -30,12 +30,13 @@ def plotseries(ensemble_output,box):
                 varnotfound = False
             axes[0,i].set_title(variables[i])
             if(mem == 0):
-                axes[0,i].plot(tims,vals,"--k",linewidth=2)
-            else:
-                axes[0,i].plot(tims,vals)
+                axes[0,i].plot(tims,vals,linewidth = 2,color = "red")
             valsarr[j,:] = vals[:]
             j += 1
         if varnotfound: continue
+        maxline = numpy.amax(valsarr,axis = 0)
+        minline = numpy.amin(valsarr,axis = 0)
+        axes[0,i].fill_between(tims,minline,maxline,facecolor = 'red',alpha = 0.5)
         mean = numpy.mean(valsarr[1:,:],0)
         std = numpy.std(valsarr,0)
         rmse = numpy.sqrt(numpy.mean((valsarr[1:,:]-valsarr[0,:])**2,0))
@@ -51,6 +52,7 @@ if __name__ == "__main__":
     parser.add_argument("--lat",dest = "lat",nargs = "+",help = "<Required> Latitude range (or value)",required = True)
     parser.add_argument("--lon",dest = "lon",nargs = "+",help = "<Required> Longitude range (or value)",required = True)
     parser.add_argument("--mul",dest = "ncstore",action = "store_const",const = "multi",default = "single" )
+    parser.add_argument("--vars",dest = "vars",nargs = "+",help = "Plot variable list",required = True)
     args = parser.parse_args()
     path,lat,lon = args.path,args.lat,args.lon
     box = None
@@ -65,4 +67,4 @@ if __name__ == "__main__":
         ensemble = ensoutput.singlenetcdf(path)
     elif(args.ncstore == "multi"):
         ensemble = ensoutput.multinetcdf(path)
-    plotseries(ensemble,box)
+    plotseries(ensemble,box,args.vars)
