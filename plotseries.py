599d2be4773dd2d31a4466d503dd0c6635cf8e71
Gijs van den Oord <nkgo@ecgb11.ecmwf.int>
2017-09-12 13:07:36 +0000

    Added statistics plots, fixed time axis

--- plotseries.py
+++ plotseries.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
+from matplotlib.dates import DayLocator,DateFormatter
 import argparse
 import numpy
 import ensoutput
+import datetime
 
 def plotseries(ensemble_output,box,pltvars):
     if(not ensemble_output):
@@ -12,10 +14,11 @@ def plotseries(ensemble_output,box,pltvars):
     if(len(members) < 1):
         print "No ensemble members found in " + ensemble_output.path
     variables = [v for v in pltvars if v in ensemble_output.get_variables()]
-    fig,axes = plt.subplots(2,len(variables))
+    fig,axes = plt.subplots(3,len(variables))
     for i in range(len(variables)):
         axes[0,i].ticklabel_format(style='sci', axis='y', scilimits=(-2,2))
         axes[1,i].ticklabel_format(style='sci', axis='y', scilimits=(-2,2))
+        axes[2,i].ticklabel_format(style='sci', axis='y', scilimits=(-2,2))
         tims = ensemble_output.get_times(variables[i],1)
         valsarr = numpy.zeros([len(members),len(tims)])
         varnotfound = False
@@ -31,6 +34,7 @@ def plotseries(ensemble_output,box,pltvars):
             axes[0,i].set_title(variables[i])
             if(mem == 0):
                 axes[0,i].plot(tims,vals,linewidth = 2,color = "red")
+                axes[1,i].plot(tims,vals,linewidth = 2,color = "red")
             valsarr[j,:] = vals[:]
             j += 1
         if varnotfound: continue
@@ -41,9 +45,20 @@ def plotseries(ensemble_output,box,pltvars):
         std = numpy.std(valsarr,0)
         rmse = numpy.sqrt(numpy.mean((valsarr[1:,:]-valsarr[0,:])**2,0))
         axes[0,i].plot(tims,mean,"k",linewidth=1)
-        stdline, = axes[1,i].plot(tims,std,label = "spread")
-        rmseline, = axes[1,i].plot(tims,rmse,label = "rmse")
-        axes[1,i].legend(handles = [stdline,rmseline])
+        axes[1,i].plot(tims,mean,"k",linewidth=1)
+        stdplus,stdmin = mean + std,mean - std
+        axes[1,i].fill_between(tims,stdplus,stdmin,facecolor = 'black',alpha = 0.25)
+        stdline, = axes[2,i].plot(tims,std,label = "spread")
+        rmseline, = axes[2,i].plot(tims,rmse,label = "rmse")
+        axes[2,i].legend(handles = [stdline,rmseline])
+        days = DayLocator()
+        xformat = DateFormatter("%d-%m")
+        evtdate = tims[0] + datetime.timedelta(days = 5)
+        for k in [0,1,2]:
+            axes[k,i].xaxis.set_major_locator(days)
+            axes[k,i].xaxis.set_major_formatter(xformat)
+            axes[k,i].axvspan(evtdate,evtdate + datetime.timedelta(days=1),color = "blue",alpha = 0.3)
+    fig.autofmt_xdate()
     plt.show()
 
 if __name__ == "__main__":
