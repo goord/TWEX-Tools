22a7f0c90e1ac65862a09f10f097067a695dfd1e
Gijs van den Oord <nkgo@ecgb11.ecmwf.int>
2017-10-23 19:36:48 +0000

    Fixed data retrieval script

--- geteipr.py
+++ geteipr.py
@@ -1,10 +1,19 @@
+#!/bin/env python
+
 import os
+import argparse
 import calendar
+import datetime
 import ecmwfapi
 import utils
 
 def retrieve_prmon(mon,eifile,box = None):
-    dx = 1.25
+    dx = 0.75
+    startyear,endyear = 1980,2015
+    dates = []
+    for y in range(startyear,endyear + 1):
+        numdays = calendar.monthrange(y,mon)[1]
+        dates.extend([datetime.date(y,mon,d) for d in range(1,numdays + 1)])
     if(not os.path.isfile(eifile)):
         request = {"stream"  : "oper",
                    "levtype" : "sfc",
@@ -12,19 +21,19 @@ def retrieve_prmon(mon,eifile,box = None):
                    "dataset" : "interim",
                    "step"    : "3/6/9/12",
                    "grid"    : '/'.join([str(dx),str(dx)]),
-                   "time"    : "00/06/12/18",
-                   "date"    : calendar.month_name[mon],
-                   "type"    : "an",
+                   "time"    : "00:00:00/12:00:00",
+                   "date"    : '/'.join([d.strftime("%Y-%m-%d") for d in dates]),
+                   "type"    : "fc",
                    "class"   : "ei",
                    "format"  : "netcdf",
                    "target"  : eifile
                    }
         if(box):
             extent = []
-            if(len(box) == 1):
+            if(len(box) == 2):
                 extent = [box[0] + dx,box[1] - dx,box[0] - dx,box[1] + dx]
             else:
-                extent = [box[2],box[1],box[0],box[3]]
+                extent = [box[1],box[2],box[0],box[3]]
             request["area"] = '/'.join([str(c) for c in extent])
         print "The request is",request
         server = ecmwfapi.ECMWFDataServer()
@@ -37,7 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("--path",dest = "path",help = "<Required> Target location",required = True)
     parser.add_argument("--loc",dest = "loc",nargs = "+",help = "<Required> Data location: either WestCoast, Svalbard or "
                                                                 "a lat-lon pair or a lat-lon box (latmin latmax lonmin lonmax)")
-    parser.add_argument("--mon",dest = mon,help = "<Required> Month 1-12",required = True)
+    parser.add_argument("--mon",dest = "mon",type = int,help = "<Required> Month 1-12",required = True)
     args = parser.parse_args()
     path,loc,mon = args.path,getattr(args,"loc",None),args.mon
     box = utils.getbox(loc) if loc else None
